4907f17222e2fac04cfdd7ace0eef8cb6151ceaf
Mark Gandolfo <mark@aussiev8.com.au>
2013-04-04 20:05:33 +1000

    updating files

---
+++ shell/aliases/main.sh
@@ -0,0 +1,22 @@
+alias _='sudo'
+alias please='sudo'
+
+alias reload="source $HOME/.zshrc"            # Source dotfiles
+alias sauce='reload'                          # Meta alias!
+alias history='fc -l 1'                       # I want to see more the 16 history items
+alias z="_z -r 2>&1"                          # Highest rated results first
+alias internet\?='ping 8.8.8.8'               # Google?
+
+alias b='bundle && say bundle complete, mate' # Run bundle and say when it's done
+
+alias dev="cd $HOME/Dev"                      # Go to the Dev directory
+alias vimdir="cd $HOME/.dotfiles/vim"         # Go to the Vim directory
+alias dotdir="cd $HOME/.dotfiles"             # Go to the Dotfiles directory
+
+d() {
+  if [[ -n "$1" ]]; then
+    cd "+$1"
+  else
+    dirs -v
+  fi
+}
