855d3a849f0cb4eff7b742a5442f5f9fed776329
Mark Gandolfo <mark@aussiev8.com.au>
2013-12-04 13:52:11 +1000

    added mysql shortcuts and fixed postgres

--- shell/aliases/shortcuts.zsh
+++ shell/aliases/shortcuts.zsh
@@ -1,2 +1,10 @@
+# Mongo DB
 alias mongodbstart='mongod --dbpath /data/db/'
-alias pgstart -D ~/Sites/postgres -l ~/Sites/postgres/logs/server.log start
+
+# Postgres
+alias pgstart='pg_ctl -D ~/Sites/postgres -l ~/Sites/postgres/logs/server.log start'
+alias pgstop='pg_ctl -D ~/Sites/postgres stop'
+
+# Mysql
+alias mysqlstart='/usr/local/bin/mysqld start'
+alias mysqlstop='/usr/local/bin/mysqld stop'
