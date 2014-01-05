e4457cd486d48c6a0bdbef6c9fda09eae78a8025
Mark Gandolfo <mark@aussiev8.com.au>
2014-01-06 07:16:31 +1000

    adding some emacs love

---
+++ emacs.d/init.el
@@ -0,0 +1,11 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(load "~/.emacs.d/user.el")
