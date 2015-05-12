116c01c37d23f1f4001203c92b665dd45966e050
Mark Gandolfo <mark@gandolfo.com.au>
2015-05-13 09:44:47 +1000

    rename eyeballs and add alias for togglr states

--- shell/aliases/hooroo.zsh
+++ shell/aliases/hooroo.zsh
@@ -1,3 +1,4 @@
-alias eyeballjetstar='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=jetstar'
-alias eyeballqantas='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=qantas'
+alias ebj='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=jetstar'
+alias ebq='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=qantas'
+alias ts='bundle exec togglr states'
 
