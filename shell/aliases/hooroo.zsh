f9dd3956d18194727d84f71efd90c360cdc03092
Mark Gandolfo <mark@gandolfo.com.au>
2015-05-19 14:32:07 +1000

    foreman start and lolcat

--- shell/aliases/hooroo.zsh
+++ shell/aliases/hooroo.zsh
@@ -1,4 +1,4 @@
-alias ebj='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=jetstar'
-alias ebq='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=qantas'
+alias ebj='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=jetstar | lolcat'
+alias ebq='rake email_eyeball:all\[mark.gandolfo@hooroo.com\] source=qantas | lolcat'
 alias ts='bundle exec togglr states'
 
--- shell/aliases/rails.zsh
+++ shell/aliases/rails.zsh
@@ -1,2 +1,3 @@
 alias be='bundle exec'
 alias becd='bundle exec cap deploy'
+alias fs='foreman start'
