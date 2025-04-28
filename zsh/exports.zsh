6cba58362138802f849a0518716164a899950840
Mark Gandolfo <mark@gandolfo.com.au>
2025-04-28 15:40:12 +1000

    add conditional nvm export

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,1 +1,10 @@
 export EDITOR="nvim"
+
+# Check if the NVM directory exists before setting NVM_DIR
+[ -d "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"
+
+# Only try to load NVM if NVM_DIR is set
+if [ -n "$NVM_DIR" ]; then
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+fi
